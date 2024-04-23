2c28c150895ab0aa5354786209f63a58e7e17dcd
Gabriel Divtech <gabriel.lira@divtech.com.br>
2024-04-23 08:55:23 -0300

    feature: initial commit

---
+++ src/test/java/com/app/museumchatbot/MuseumchatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.museumchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumchatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
