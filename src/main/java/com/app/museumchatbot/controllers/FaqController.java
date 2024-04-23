9df0616b342964d683cd7c06c8c5df5a0942bb1e
Gabriel Divtech <gabriel.lira@divtech.com.br>
2024-04-23 15:44:45 -0300

    refactor: removed unused import

--- src/main/java/com/app/museumchatbot/controllers/FaqController.java
+++ src/main/java/com/app/museumchatbot/controllers/FaqController.java
@@ -1,14 +1,10 @@
 package com.app.museumchatbot.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.app.museumchatbot.dto.MessageRequest;
 import com.app.museumchatbot.dto.MessageResponse;
 import com.app.museumchatbot.services.FaqService;
-import com.app.museumchatbot.utils.FaqAnswers;
 
 @RestController
 @RequestMapping("api/chat")
