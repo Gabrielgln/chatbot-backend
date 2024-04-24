cd1835d63694577537bac7ffc4ee2ab3026e955e
Gabriel Divtech <gabriel.lira@divtech.com.br>
2024-04-23 22:47:52 -0300

    feat: add string treatment before matching keyword

--- src/main/java/com/app/museumchatbot/services/FaqService.java
+++ src/main/java/com/app/museumchatbot/services/FaqService.java
@@ -1,7 +1,10 @@
 package com.app.museumchatbot.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.app.museumchatbot.domain.FaqAnswer;
 import com.app.museumchatbot.utils.FaqAnswers;
 
 @Service
@@ -10,9 +13,13 @@ public class FaqService {
 
     public String getAnswer(String question){
         String[] words = question.toLowerCase().split("\\s+");
-        for (int i = 0; i < words.length; i++) {
-            if(faqAnswers.getAnswers().get(i).getKeywords().contains(words[i].toLowerCase())){
-                return faqAnswers.getAnswers().get(i).getAnswer();
+
+        for (int i = 0; i < faqAnswers.getAnswers().size(); i++) {
+            List<String> keywords = faqAnswers.getAnswers().get(i).getKeywords();
+            for(String word : words){
+                if(keywords.contains(word)){
+                    return faqAnswers.getAnswers().get(i).getAnswer();
+                }
             }
         }
         return faqAnswers.getDefaultAnswer();
